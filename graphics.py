32c2388da442100d9bc63f62ae731449a625f2a9
oakinh <nikaohill@gmail.com>
2024-06-27 16:20:38 -0600

    Write redraw, wait_for_close, and close methods

---
+++ graphics.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, width=width, height=height, bg="white")
+        self.__canvas.pack(fill=BOTH, expand=True)
+        self.__isrunning = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.__isrunning = True
+        while self.__isrunning:
+            self.redraw()
+        print("Window closed...")
+    
+    def close(self):
+        self.__isrunning = False
