68d640326465e3dc4f79076ace5ff2e2814635f1
oakinh <nikaohill@gmail.com>
2024-06-27 18:42:27 -0600

    Add draw_move method for Cell class

--- cell.py
+++ cell.py
@@ -12,7 +12,6 @@ class Cell:
         self._y2 = None
         self._win = win
 
-
     def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._x2 = x2
@@ -30,3 +29,20 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        fill_color = None
+        if not undo:
+            fill_color = "red"
+        else:
+            fill_color = "gray"
+        self_center_x = (self._x1 + self._x2) // 2
+        self_center_y = (self._y1 + self._y2) // 2
+
+        to_center_x = (to_cell._x1 + to_cell._x2) // 2
+        to_center_y = (to_cell._y1 + to_cell._y2) // 2
+
+        line = Line(Point(self_center_x, self_center_y), Point(to_center_x, to_center_y))
+
+        self._win.draw_line(line, fill_color)
+
